ec9e009a490ad5f2ccae7036ad803b2e1ad1e481
Alex Olds <44590110+apo6377@users.noreply.github.com>
2019-04-10 10:43:39 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/pokecmd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pokecmd.dir/main.cpp.o"
+  "pokecmd.pdb"
+  "pokecmd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pokecmd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
